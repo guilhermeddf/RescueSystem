b93883fb2de4c60ce35a960d03c8bb7107a5c45f
guilhermeddf <guilhermedin@gmail.com>
2021-04-19 08:48:52 -0300

    initial commmit

--- src/main/java/br/ufrn/extension/gatinhos/rescuesystem/cat/CatStatus.java
+++
@@ -1,5 +0,0 @@
-package br.ufrn.extension.gatinhos.rescuesystem.cat;
-
-public enum CatStatus {
-    SAUDAVEL, DOENTE
-}
--- src/main/java/br/ufrn/extension/gatinhos/rescuesystem/rescue/RescueRepository.java
+++
@@ -1,6 +0,0 @@
-package br.ufrn.extension.gatinhos.rescuesystem.rescue;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RescueRepository extends MongoRepository<Rescue, String> {
-}
